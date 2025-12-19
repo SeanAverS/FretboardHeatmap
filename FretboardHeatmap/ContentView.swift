a89b35f774cbbcaddea293211a59419786958dd4
Sean Suguitan <seanasuguitan@gmail.com>
2025-12-18 16:56:43 -0800

    Initial Commit

---
+++ FretboardHeatmap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FretboardHeatmap
+//
+//  Created by Sean Avery Suguitan on 2025-12-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
