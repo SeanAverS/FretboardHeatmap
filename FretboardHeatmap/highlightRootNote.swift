fe0177a3645d7c267004e1d7d88aec082a415234
Sean Suguitan <seanasuguitan@gmail.com>
2026-01-29 17:23:33 -0800

    refactor: turn root dictionary into Constant for less compiler load

--- FretboardHeatmap/highlightRootNote.swift
+++ FretboardHeatmap/highlightRootNote.swift
@@ -12,54 +12,53 @@
 
 import Foundation
 
+// MARK: Major Chords
+private let roots: [String: [Int: [Int]]] = [
+    "G": [
+        0: [3],
+        1: [8],
+        2: [0, 12],
+        3: [5],
+        4: [10],
+        5: [3]
+         ],
+    "D": [
+        0: [10],
+        1: [3],
+        2: [7],
+        3: [0, 12],
+        4: [5],
+        5: [10]
+         ],
+    "C": [
+        0: [8],
+        1: [1],
+        2: [5],
+        3: [10],
+        4: [3],
+        5: [8]
+         ],
+    "E": [
+        0: [0, 12],
+        1: [5],
+        2: [9],
+        3: [2],
+        4: [7],
+        5: [0, 12]
+         ],
+    "A": [
+        0: [5],
+        1: [10],
+        2: [2],
+        3: [7],
+        4: [0, 12],
+        5: [5]
+         ]
+]
+
 struct HighlightRootNote {
     static func check(root: String, string: Int, fret: Int) -> Bool {
-        switch root {
-        case "G":
-            return (string == 2 && fret == 0) ||
-            (string == 0 && fret == 3) ||
-            (string == 5 && fret == 3) ||
-            (string == 3 && fret == 5) ||
-            (string == 1 && fret == 8) ||
-            (string == 4 && fret == 10) ||
-            (string == 2 && fret == 12)
-            
-        case "D":
-            return (string == 3 && fret == 0) ||
-            (string == 1 && fret == 3) ||
-            (string == 4 && fret == 5) ||
-            (string == 2 && fret == 7) ||
-            (string == 0 && fret == 10) ||
-            (string == 5 && fret == 10) ||
-            (string == 3 && fret == 12)
-            
-        case "C":
-            return (string == 1 && fret == 1) ||
-            (string == 4 && fret == 3) ||
-            (string == 2 && fret == 5) ||
-            (string == 0 && fret == 8) ||
-            (string == 5 && fret == 8) ||
-            (string == 3 && fret == 10)
-            
-        case "E":
-            return (string == 3 && fret == 2) ||
-            (string == 1 && fret == 5) ||
-            (string == 4 && fret == 7) ||
-            (string == 2 && fret == 9) ||
-            (string == 0 && fret == 12) ||
-            (string == 5 && fret == 12)
-            
-        case "A":
-            return (string == 4 && fret == 0) ||
-            (string == 2 && fret == 2) ||
-            (string == 0 && fret == 5) ||
-            (string == 5 && fret == 5) ||
-            (string == 3 && fret == 7) ||
-            (string == 1 && fret == 10) ||
-            (string == 4 && fret == 12)
-            
-        default:
-            return false
-        }
+        // if the current string contains a root note
+        return roots[root]?[string]?.contains(fret) ?? false
     }
 }
