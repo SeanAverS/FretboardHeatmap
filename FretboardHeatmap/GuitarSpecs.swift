ae72bb10dd7b6670211b20ac28828ef51236f992
Sean Suguitan <seanasuguitan@gmail.com>
2026-02-17 16:29:58 -0800

    docs: GuitarSpecs documentation

--- FretboardHeatmap/GuitarSpecs.swift
+++ FretboardHeatmap/GuitarSpecs.swift
@@ -4,13 +4,18 @@
 //
 //  Created by Sean Avery Suguitan on 2026-01-25.
 //
-//  static data for guitar parts/root 
 
 import Foundation
 
+///  Provides data for guitar frets, strings, and root notes
 struct GuitarSpecs {
+    // guitar frets
     static let frets: [CGFloat] = [90, 85, 80, 75, 71, 67, 63, 60, 56, 53, 50, 47]
+    
+    // guitar strings
     static let strings: [CGFloat] = [0.8, 1.2, 1.8, 2.5, 3.2, 4.0]
+    
+    // root notes
     static let roots = ["G", "D", "C", "E", "A"]
 }
 
