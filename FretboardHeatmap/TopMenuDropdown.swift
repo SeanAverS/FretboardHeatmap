59abd97c94c4e25af7771d033777413d508d20af
Sean Suguitan <seanasuguitan@gmail.com>
2026-02-18 17:33:11 -0800

    docs: TopMenuDropdown documentation

--- FretboardHeatmap/TopMenuDropdown.swift
+++ FretboardHeatmap/TopMenuDropdown.swift
@@ -4,16 +4,20 @@
 //
 //  Created by Sean Avery Suguitan on 2026-01-17.
 //
-//  Renders a dropdown for chord and scale options
 
 import SwiftUI
 
+/// Prepare dropdown for top menu area
+/// - Parameters:
+///    - selectedDropdownOption: The current dropdown choice
+///    - activeMenu: Top menu choice
+/// - Returns: Dropdown for right top menu choice
 struct TopMenuDropdown: View {
     @Binding var selectedDropdownOption: String
     let activeMenu: menuChoice?
     
     var body: some View {
-        Menu { // Dropdown menu
+        Menu { // Dropdown
             if let activeMenu = activeMenu {
                 ForEach(FretPositions.dropdownChoices(for: activeMenu), id: \.self) { dropdownChoice in
                     Button(dropdownChoice) {
@@ -21,7 +25,7 @@ struct TopMenuDropdown: View {
                     }
                 }
             }
-        } label: { // Dropdown labels
+        } label: { // Styling
             HStack(spacing: 5) {
                 Text(selectedDropdownOption.uppercased())
                     .font(.system(size: 17, weight: .bold))
